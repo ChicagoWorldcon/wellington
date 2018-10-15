e8f28c1d3f1c793de4abad952ec9f4baa178de9e
Matthew B. Gray <hey@matthew.nz>
2018-10-15 18:11:35 +1300

    Create user model, issue #3

--- db/schema.rb
+++ db/schema.rb
@@ -25,4 +25,11 @@ ActiveRecord::Schema.define(version: 20181006225322) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email"
+  end
+
 end
