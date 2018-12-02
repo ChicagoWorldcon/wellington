07ac03af38083061f92e7642b52232c5a5f6a667
ajesler <aj@esler.co.nz>
2018-12-02 18:05:46 +1300

    Add kansa member csv importer
    This is based on the presupporters importer, but does some things a bit
    differently, eg Charges.

---
+++ app/commands/import_kansa_members_row.rb
@@ -0,0 +1,117 @@
+# Copyright 2018 Matthew B. Gray
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+class ImportKansaMembersRow
+  HEADINGS = [
+    "Full name",
+    "PreferredFirstname",
+    "PreferedLastname",
+    "BadgeTitle",
+    "BadgeSubtitle",
+    "Address Line1",
+    "Address Line2",
+    "City",
+    "Province/State",
+    "Postal/Zip Code",
+    "Country",
+    "Email Address",
+    "Use Real Name",
+    "Use Badge",
+    "Share detalis?",
+    "Share With Future Worldcons",
+    "No electronic publications",
+    "Paper Publications",
+    "Volunteering",
+    "Accessibility Services",
+    "Being on Program",
+    "Dealers",
+    "Selling at Art Show",
+    "Exhibiting",
+    "Performing",
+    "Notes",
+    "Import Key",
+    "Membership Status",
+    "Stripe Payment ID",
+    "Charge Amount",
+    "Payment Comment",
+    "Membership Number",
+  ].freeze
+
+  MEMBERSHIP_LOOKUP = {
+    "Adult Attending":          "adult",
+    "Child Attending":          "child",
+    "Kiwi Pre-Support":         "kiwi",
+    "Pre-Opposing":             "pre_oppose",
+    "Pre-Supporting":           "pre_support",
+    "Silver Fern Pre-Support":  "silver_fern",
+    "Supporting":               "supporting",
+    "Tuatara Pre-Support":      "tuatara",
+    "Young Adult Attending":    "young_adult",
+  }.with_indifferent_access.freeze
+
+  attr_reader :row_data, :comment
+
+  def initialize(row_data, comment)
+    @row_data = row_data
+    @comment = comment
+  end
+
+  def call
+    new_user = User.new(email: cell_for("Email Address"))
+    if !new_user.valid?
+      errors << new_user.errors.full_messages.to_sentence
+      return false
+    end
+
+    command = PurchaseMembership.new(membership, customer: new_user)
+    new_purchase = command.call
+
+    if !new_purchase
+      errors << command.error_message
+      return false
+    end
+
+    new_purchase.update!(state: Purchase::PAID)
+    Charge.stripe.successful.create!(
+      user: new_user,
+      purchase: new_purchase,
+      amount: cell_for("Charge Amount"),
+      stripe_id: cell_for("Stripe Payment ID"),
+      comment: cell_for("Payment Comment"),
+    )
+
+    errors.none?
+  end
+
+  def errors
+    @errors ||= []
+  end
+
+  def error_message
+    errors.to_sentence
+  end
+
+  private
+
+  def membership
+    import_string = cell_for("Membership Status")
+    membership_name = MEMBERSHIP_LOOKUP[import_string] || import_string
+    Membership.find_by!(name: membership_name)
+  end
+
+  def cell_for(column)
+    offset = HEADINGS.index(column)
+    row_data[offset]
+  end
+end
