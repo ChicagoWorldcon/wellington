396a1cc3a9b784974aa750a9ab78f6ac99380a25
Matthew B. Gray <hey@matthew.nz>
2019-01-09 07:38:52 +1300

    issue #15: Setup basic feature tests against LoginTokensController#new

--- spec/controllers/login_tokens_controller_spec.rb
+++ spec/controllers/login_tokens_controller_spec.rb
@@ -1,5 +1,30 @@
-require 'rails_helper'
+# frozen_string_literal: true
 
-RSpec.describe LoginTokensController, type: :controller do
+# Copyright 2019 Matthew B. Gray
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+require "rails_helper"
+
+RSpec.describe LoginTokensController, type: :feature do
+  let(:email_input) { "input[name=email]" }
+  let(:submit_button) { "input[type=submit]" }
+
+  describe "#new" do
+    it "lets me sign in" do
+      visit "/login_tokens/new"
+      expect(page).to have_css(email_input)
+      expect(page).to have_css(submit_button)
+    end
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Copyright 2018 Matthew B. Gray
+# Copyright 2019 Matthew B. Gray
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -24,8 +24,9 @@ require File.expand_path("../../config/environment", __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 
-require "rspec/rails"
 # Add additional requires below this line. Rails is not loaded until this point!
+require "capybara/rails"
+require "rspec/rails"
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
