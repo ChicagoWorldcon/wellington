8b4c9d187ef24cda76b76d3dcb296c32bdfebec8
Matthew B. Gray <hey@matthew.nz>
2019-01-12 13:25:42 +1300

    issue #15: Create Token::SendLink
    Working on removing responsibilities from LoginToken

---
+++ app/commands/token/send_link.rb
@@ -0,0 +1,70 @@
+# frozen_string_literal: true
+
+# Copyright 2019 Matthew B. Gray
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+class Token::SendLink
+  TOKEN_DURATION = 10.minutes
+
+  attr_reader :email
+  attr_reader :secret
+
+  def initialize(email:, secret:)
+    @email = email
+    @secret = secret
+  end
+
+  def call
+    check_email
+    check_secret
+
+    encode_token if errors.none?
+    email_link if errors.none?
+    errors.none?
+  end
+
+  def errors
+    @errors ||= []
+  end
+
+  private
+
+  def check_email
+    if !email.present?
+      errors << "email address missing"
+    elsif !email.match(Devise.email_regexp)
+      errors << "email format invalid"
+    end
+  end
+
+  def check_secret
+    if !secret.present?
+      errors << "cannot encode without secret"
+    end
+  end
+
+  def encode_token
+    token_data = {
+      exp: (Time.now + TOKEN_DURATION).to_i,
+      email: email,
+    }
+    @token = JWT.encode(token_data, secret, "HS256")
+  rescue JWT::EncodeError
+    errors << "failed to encode JWT token"
+  end
+
+  def email_link
+    MembershipMailer.login_link(email: email, token: @token)
+  end
+end
---
+++ spec/commands/token/send_link_spec.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+# Copyright 2019 Matthew B. Gray
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require "rails_helper"
+
+RSpec.describe Token::SendLink do
+  let(:good_email) { Faker::Internet.email }
+  let(:good_secret) { "you'll never find the treasure" }
+  let(:query) { Token::SendLink.new(email: good_email, secret: good_secret) }
+
+  describe "#call" do
+    it "sends email" do
+      service = Token::SendLink.new(email: good_email, secret: good_secret)
+      expect(MembershipMailer).to receive(:login_link).and_return(true)
+      expect(service.call).to be_truthy
+    end
+
+    context "with invalid inptus" do
+      before { expect(MembershipMailer).to_not receive(:login_link) }
+
+      it "fails with bad email" do
+        [
+          Token::SendLink.new(email: "", secret: good_secret),
+          Token::SendLink.new(email: "hax0r", secret: good_secret),
+        ].each do |service|
+          expect(service.call).to be_falsey
+          expect(service.errors).to include(/email/i)
+        end
+      end
+
+      it "fails with missing secret" do
+        service = Token::SendLink.new(email: good_email, secret: "")
+        expect(service.call).to be_falsey
+        expect(service.errors).to include(/secret/i)
+      end
+
+      it "reports errors when JWT encode fails" do
+        expect(JWT).to receive(:encode).and_raise(JWT::EncodeError)
+        service = Token::SendLink.new(email: good_email, secret: good_secret)
+        expect(service.call).to be_falsey
+        expect(service.errors).to include(/jwt/i)
+      end
+    end
+  end
+end
