e3c130d544d050673b30518831e0259489b13550
Steven C Hartley <Steven.C.Hartley@gmail.com>
2019-03-01 22:05:58 +1300

    changing hash syntax to match Ruby 1.9 and make rubocop happy

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,11 +72,11 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  
+
   #mail catcher
   #config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => '0.0.0.0', :port => 1025 }
+  config.action_mailer.smtp_settings = { address: "0.0.0.0", port: 1025 }
 
 
 end
