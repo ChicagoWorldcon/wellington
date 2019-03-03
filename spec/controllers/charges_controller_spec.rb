13119f4ba6f4101da98548fa225023c30fdf3033
Matthew B. Gray <hey@matthew.nz>
2019-03-03 14:21:31 +1300

    issue #29: Update copyright year in apache boilerplate

--- spec/controllers/charges_controller_spec.rb
+++ spec/controllers/charges_controller_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-# Copyright 2018 Matthew B. Gray
 # Copyright 2019 AJ Esler
+# Copyright 2019 Matthew B. Gray
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
