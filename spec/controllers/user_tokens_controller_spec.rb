7bec8d3df0b9987c5fc19ab057fabba1b2b2042c
Steven C Hartley <Steven.C.Hartley@gmail.com>
2019-04-18 21:50:12 +1200

    Changing to use the secret method of the controller on rspec

--- spec/controllers/user_tokens_controller_spec.rb
+++ spec/controllers/user_tokens_controller_spec.rb
@@ -28,8 +28,8 @@ RSpec.describe UserTokensController, type: :controller do
     let(:user_token) { "asdf" }
     let(:good_email) { "willy_wönka@chocolate_factory.nz" }
     let(:valid_login_path) { "/purchases/new" }
-    let(:valid_login_path_token) { JWT.encode({exp: (Time.now + 10.minutes).to_i, email: good_email, path: "/purchases/new", }, ENV["JWT_SECRET"], "HS256") }
-    let(:invalid_login_path_token) { JWT.encode({exp: (Time.now + 10.minutes).to_i, email: good_email, path: "/notarealpath", }, ENV["JWT_SECRET"], "HS256") }
+    let(:valid_login_path_token) { JWT.encode({exp: (Time.now + 10.minutes).to_i, email: good_email, path: "/purchases/new", }, secret, "HS256") }
+    let(:invalid_login_path_token) { JWT.encode({exp: (Time.now + 10.minutes).to_i, email: good_email, path: "/notarealpath", }, secret, "HS256") }
 
     context "when secret is not set" do
       before do
