900ea392b38c95543ed0b08c42cada32220fed05
Matthew B. Gray <hey@matthew.nz>
2019-04-28 16:27:48 +1200

    issue #83: Speed up memberships listing page
    I read a great article about preloading here:
    https://blog.arkency.com/2013/12/rails4-preloading/

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -25,10 +25,6 @@ class PurchasesController < ApplicationController
       @my_purcahses = @my_purcahses.joins(:membership)
       @my_purcahses = @my_purcahses.includes(:charges).includes(active_claim: :detail)
     end
-
-    if support_signed_in?
-      @everyones_purchases = Purchase.includes(:user).joins(:membership)
-    end
   end
 
   def new
