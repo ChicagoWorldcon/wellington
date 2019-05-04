7fffe2379c0f6f1718d4d9e95cf9f16ee318549c
jenofdoom <j_zajac@email.com>
2019-05-04 12:54:01 +1200

    issue #77: code review fixups

--- app/queries/membership_offer.rb
+++ app/queries/membership_offer.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 # Copyright 2019 Matthew B. Gray
+# Copyright 2019 Jen Zajac (jenofdoom)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
