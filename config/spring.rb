bfc7b3fea79ed63c8cf9e2f8f182a1b37e7494dc
Matthew B. Gray <matthew.gray@storypark.com>
2019-05-23 22:01:05 +1200

    maint: Add .env to spring watch so things reload properly

--- config/spring.rb
+++ config/spring.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Copyright 2018 Matthew B. Gray
+# Copyright 2019 Matthew B. Gray
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@
 %w(
   .ruby-version
   .rbenv-vars
+  .env
   tmp/restart.txt
   tmp/caching-dev.txt
 ).each { |path| Spring.watch(path) }
