51ad149de7eed5f5a898dae7cbefc79879db0bc9
Matthew B. Gray <hey@matthew.nz>
2019-07-18 18:09:07 +1200

    issue #96: Create and display Hugo categories from Dev seeds
    Just want to get a feel for how it looks, getting there slowly

--- db/seeds/conzealand/development.seeds.rb
+++ db/seeds/conzealand/development.seeds.rb
@@ -18,15 +18,14 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
+# Setup memberships
 announcement = Date.parse("2018-08-25").midday
 presupport_start = announcement - 2.years
-
 FactoryBot.create(:membership, :silver_fern , active_from: presupport_start, active_to: announcement)
 FactoryBot.create(:membership, :kiwi        , active_from: presupport_start, active_to: announcement)
 FactoryBot.create(:membership, :tuatara     , active_from: presupport_start, active_to: announcement)
 FactoryBot.create(:membership, :pre_oppose  , active_from: presupport_start, active_to: announcement)
 FactoryBot.create(:membership, :pre_support , active_from: presupport_start, active_to: announcement)
-
 FactoryBot.create(:membership, :adult       , active_from: announcement)
 FactoryBot.create(:membership, :young_adult , active_from: announcement)
 FactoryBot.create(:membership, :unwaged     , active_from: announcement)
@@ -34,12 +33,14 @@ FactoryBot.create(:membership, :child       , active_from: announcement)
 FactoryBot.create(:membership, :kid_in_tow  , active_from: announcement)
 FactoryBot.create(:membership, :supporting  , active_from: announcement)
 
+# Setup users
 all_memberships = Membership.all.to_a
 
 50.times do |count|
   puts "Seeding #{count} of 50 users" if count % 5 == 0
   new_user = FactoryBot.create(:user)
   memberships_held = rand(2..10)
+
   all_memberships.sample(memberships_held).each do |rando_membership|
     if rando_membership.price == 0
       state = Reservation::PAID
@@ -47,8 +48,9 @@ all_memberships = Membership.all.to_a
       state = [Reservation::PAID, Reservation::INSTALMENT].sample
     end
 
-    reservation = FactoryBot.create(:reservation, user: new_user, membership: rando_membership, state: state)
+    FactoryBot.create(:reservation, user: new_user, membership: rando_membership, state: state)
   end
+
   new_user.active_claims.each do |claim|
     claim.update!(detail: FactoryBot.create(:detail, claim: claim))
   end
@@ -61,3 +63,18 @@ Support.create(
   password: 111111,
   confirmed_at: Time.now,
 )
+
+# Hugo setup
+FactoryBot.create(:category, :best_novel)
+FactoryBot.create(:category, :best_novella)
+FactoryBot.create(:category, :best_novelette)
+FactoryBot.create(:category, :best_short_story)
+FactoryBot.create(:category, :best_related_work)
+FactoryBot.create(:category, :best_graphic_story)
+FactoryBot.create(:category, :best_dramatic_presentation_long_form)
+FactoryBot.create(:category, :best_dramatic_presentation_short_form)
+FactoryBot.create(:category, :best_editor_long_form)
+FactoryBot.create(:category, :best_editor_short_form)
+FactoryBot.create(:category, :best_professional_artist)
+FactoryBot.create(:category, :best_semiprozine)
+FactoryBot.create(:category, :john_w_campbell_award)
