ec7e09f46a8d62fc906a24f21b56aec08641588e
Matthew B. Gray <matthew.gray@storypark.com>
2019-08-08 07:58:37 +1200

    issue #117: Kiosk mode can only view memberships created in the last hour

--- spec/controllers/kiosk/next_steps_controller_spec.rb
+++ spec/controllers/kiosk/next_steps_controller_spec.rb
@@ -22,7 +22,14 @@ RSpec.describe Kiosk::NextStepsController, type: :controller do
   before { session[:kiosk] = 1.minute.from_now }
 
   describe "#index" do
-    let!(:reservation) { create(:reservation, :with_order_against_membership, user: member_services_user) }
+    let!(:reservation) do
+      create(:reservation, :with_order_against_membership,
+        user: member_services_user,
+        created_at: 1.minute.ago,
+        updated_at: 1.minute.ago,
+      )
+    end
+
     let!(:member_services_user) { create(:user, email: $member_services_email) }
 
     subject(:get_index) do
@@ -39,6 +46,11 @@ RSpec.describe Kiosk::NextStepsController, type: :controller do
     it "finds reservation from the member_services_user" do
       get_index
       expect(response).to have_http_status(:ok)
+    end
+
+    it "can't find a reservation if it was created a while ago" do
+      reservation.update!(created_at: 2.hours.ago)
+      expect { get_index }.to raise_error(ActiveRecord::RecordNotFound)
     end
   end
 end
