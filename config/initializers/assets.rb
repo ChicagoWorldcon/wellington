e756af4e442f7e5c96ff89120aab99496d50c2ef
Matthew B. Gray <hey@matthew.nz>
2019-10-04 15:03:26 +1300

    issue #125: Remove references to dead assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -25,9 +25,5 @@ Rails.application.config.assets.version = "1.0"
 Rails.application.config.assets.paths << Rails.root.join("node_modules")
 
 # Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets
-# folder are already added.
-Rails.application.config.assets.precompile += %w(
-  bootstrap-4.2.1.min.css
-  dataTables.bootstrap4-1.10.19.min.css
-)
+# If possible, please use yarn here instead of checking in vendored assets.
+Rails.application.config.assets.precompile += %w()
