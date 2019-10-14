543090d57e1adfe390530d7c2a996a1f4593e898
Matthew B. Gray <hey@matthew.nz>
2019-10-15 07:15:31 +1300

    issue #100: Base install of vue.js in rails

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -14,6 +14,10 @@
  * limitations under the License.
  */
 
-const { environment } = require('@rails/webpacker');
+const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
 
-module.exports = environment;
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
+module.exports = environment
