38bdc3355f871bf32a6ed24b3e67298db0829ad8
Matthew B. Gray <hey@matthew.nz>
2019-11-20 18:52:03 +1300

    issue #96: Development seeds piggy back off Production seeds

--- db/seeds/conzealand/development.seeds.rb
+++ db/seeds/conzealand/development.seeds.rb
@@ -19,24 +19,11 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
 # Setup memberships
-announcement = Date.parse("2018-08-25").midday
-presupport_start = announcement - 2.years
-FactoryBot.create(:membership , :silver_fern      , active_from: presupport_start , active_to: announcement)
-FactoryBot.create(:membership , :kiwi             , active_from: presupport_start , active_to: announcement)
-FactoryBot.create(:membership , :tuatara          , active_from: presupport_start , active_to: announcement)
-FactoryBot.create(:membership , :pre_oppose       , active_from: presupport_start , active_to: announcement)
-FactoryBot.create(:membership , :pre_support      , active_from: presupport_start , active_to: announcement)
-FactoryBot.create(:membership , :adult            , active_from: announcement)
-FactoryBot.create(:membership , :young_adult      , active_from: announcement)
-FactoryBot.create(:membership , :unwaged          , active_from: announcement)
-FactoryBot.create(:membership , :child            , active_from: announcement)
-FactoryBot.create(:membership , :kid_in_tow       , active_from: announcement)
-FactoryBot.create(:membership , :supporting       , active_from: announcement)
-FactoryBot.create(:membership , :dublin_supporter , active_from: announcement)
 
-# Setup users
+puts "Running production seeds"
+require_relative "production.seeds.rb"
+
 all_memberships = Membership.all.to_a
-
 50.times do |count|
   puts "Seeding #{count} of 50 users" if count % 5 == 0
   new_user = FactoryBot.create(:user)
@@ -57,52 +44,16 @@ all_memberships = Membership.all.to_a
   end
 end
 
-# Create a default support user
-# http://localhost:3000/supports/sign_in
+puts "\nFinished creating users, try sign in with"
+puts "#{User.last.email}"
+
+puts "\nCreating a default support user"
+puts "http://localhost:3000/supports/sign_in"
+puts "user: support@worldcon.org"
+puts "pass: 111111"
+
 Support.create(
   email: "support@worldcon.org",
   password: 111111,
   confirmed_at: Time.now,
 )
-
-puts "Creating hugo awards"
-hugo_awards = FactoryBot.create(:election)
-FactoryBot.create(:category, :best_novel, election: hugo_awards)
-FactoryBot.create(:category, :best_novella, election: hugo_awards)
-FactoryBot.create(:category, :best_novelette, election: hugo_awards)
-FactoryBot.create(:category, :best_short_story, election: hugo_awards)
-FactoryBot.create(:category, :best_series, election: hugo_awards)
-FactoryBot.create(:category, :best_related_work, election: hugo_awards)
-FactoryBot.create(:category, :best_graphic_story_or_comic, election: hugo_awards)
-FactoryBot.create(:category, :best_dramatic_presentation_long_form, election: hugo_awards)
-FactoryBot.create(:category, :best_dramatic_presentation_short_form, election: hugo_awards)
-FactoryBot.create(:category, :best_professional_editor_short_form, election: hugo_awards)
-FactoryBot.create(:category, :best_professional_editor_long_form, election: hugo_awards)
-FactoryBot.create(:category, :best_professional_artist, election: hugo_awards)
-FactoryBot.create(:category, :best_semiprozine, election: hugo_awards)
-FactoryBot.create(:category, :best_fanzine, election: hugo_awards)
-FactoryBot.create(:category, :best_fancast, election: hugo_awards)
-FactoryBot.create(:category, :best_fan_writer, election: hugo_awards)
-FactoryBot.create(:category, :best_fan_artist, election: hugo_awards)
-FactoryBot.create(:category, :lodestar_award, election: hugo_awards)
-FactoryBot.create(:category, :astounding_award, election: hugo_awards)
-
-puts "Creating retro hugo awards"
-retro_hugo_awards = FactoryBot.create(:election, :retro)
-FactoryBot.create(:category, :retro_best_novel, election: retro_hugo_awards)
-FactoryBot.create(:category, :retro_best_novella, election: retro_hugo_awards)
-FactoryBot.create(:category, :retro_best_novelette, election: retro_hugo_awards)
-FactoryBot.create(:category, :retro_best_short_story, election: retro_hugo_awards)
-FactoryBot.create(:category, :retro_best_series, election: retro_hugo_awards)
-FactoryBot.create(:category, :retro_best_related_work, election: retro_hugo_awards)
-FactoryBot.create(:category, :retro_best_graphic_story_or_comic, election: retro_hugo_awards)
-FactoryBot.create(:category, :retro_best_dramatic_presentation_long_form, election: retro_hugo_awards)
-FactoryBot.create(:category, :retro_best_dramatic_presentation_short_form, election: retro_hugo_awards)
-FactoryBot.create(:category, :retro_best_professional_editor_short_form, election: retro_hugo_awards)
-FactoryBot.create(:category, :retro_best_professional_editor_long_form, election: retro_hugo_awards)
-FactoryBot.create(:category, :retro_best_professional_artist, election: retro_hugo_awards)
-FactoryBot.create(:category, :retro_best_semiprozine, election: retro_hugo_awards)
-FactoryBot.create(:category, :retro_best_fanzine, election: retro_hugo_awards)
-FactoryBot.create(:category, :retro_best_fancast, election: retro_hugo_awards)
-FactoryBot.create(:category, :retro_best_fan_writer, election: retro_hugo_awards)
-FactoryBot.create(:category, :retro_best_fan_artist, election: retro_hugo_awards)
