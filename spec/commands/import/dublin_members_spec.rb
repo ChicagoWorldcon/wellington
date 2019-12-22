85b676380f65c97b58d3bbdd22ce51207e9434ed
Matthew B. Gray <hey@matthew.nz>
2019-12-23 10:23:16 +1300

    issue #128: Initial work on Dublin imports

---
+++ spec/commands/import/dublin_members_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+# Copyright 2019 Matthew B. Gray
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require "rails_helper"
+
+RSpec.describe Import::DublinMembers do
+  let(:data) { "" }
+  let(:read_stream) { StringIO.new(data) }
+  let(:description) { "test stream" }
+  subject(:command) { described_class.new(read_stream, description) }
+
+  describe "#call" do
+    subject(:call) { command.call }
+
+    it { is_expected.to be_truthy }
+
+    it "imports nothing when the file is empty" do
+      expect { call }.to_not change { Membership.count }
+      expect(command.errors).to be_empty
+    end
+  end
+end
