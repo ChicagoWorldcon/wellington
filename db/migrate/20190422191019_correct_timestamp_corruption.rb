7271872d8915f7a2a6c4868d35cd98a6eb5430de
Matthew B. Gray <hey@matthew.nz>
2020-04-05 16:20:12 +1200

    maint: Skip conzealand specific data migration

--- db/migrate/20190422191019_correct_timestamp_corruption.rb
+++ db/migrate/20190422191019_correct_timestamp_corruption.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Copyright 2019 Matthew B. Gray
+# Copyright 2020 Matthew B. Gray
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,35 +17,8 @@
 # Imports on v1.0 had timestamps that weren't all accurate. This is an attempt to fix it.
 class CorrectTimestampCorruption < ActiveRecord::Migration[5.2]
   def up
-    imported_users = User.joins(:notes).where("notes.content LIKE ?", "Import%").distinct
-    purchases = Purchase.includes(:charges, :orders).joins(claims: :user)
-    imported_purchases = purchases.where(users: {id: imported_users})
-    imported_purchases.find_each.with_index(1) do |purchase, counter|
-      # Find the earliest claim, charge and order for the purchase
-      earliest_claim = purchase.claims.min { |c| c.created_at }
-      earliest_order = purchase.orders.min { |o| o.created_at }
-      kansa_charge = purchase.charges.find { |c| c.comment&.match(/kansa payment/i) }
-
-      # Find the earliest timestamp between all these models
-      earliest_at = [
-        purchase.created_at,
-        earliest_claim.created_at,
-        earliest_order.created_at,
-        kansa_charge&.created_at, # allow for purchases without charges ($0 purchase)
-      ].compact.min
-
-      puts "#{counter}/#{imported_purchases.count}: Setting Purchase.find(#{purchase.id}) to #{earliest_at}"
-
-      # Reset created at and active_from to be from this timestamp
-      purchase.update!(created_at: earliest_at)
-      earliest_claim.update!(created_at: earliest_at, active_from: earliest_at)
-      earliest_order.update!(created_at: earliest_at, active_from: earliest_at)
-
-      # Set earliest charge if it was close to the original purchase
-      # $0 members are skipped, e,g. child member
-      next unless kansa_charge.present?
-      kansa_charge.update!(created_at: earliest_at + 1.second)
-    end
+    # Data migration moved
+    # see db/seeds/conzealand/production_correct_timestamp_corruption.seeds.rb
   end
 
   def down
