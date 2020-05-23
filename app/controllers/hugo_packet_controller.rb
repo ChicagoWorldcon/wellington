f373f64536b96d66b0f92c279698ca39b9a70e51
Matthew B. Gray <hey@matthew.nz>
2020-05-24 10:11:04 +1200

    issue #131: Whitespace refator, 2 spaces and indentation in html

--- app/controllers/hugo_packet_controller.rb
+++ app/controllers/hugo_packet_controller.rb
@@ -13,10 +13,9 @@
 # limitations under the License.
 
 class HugoPacketController < ApplicationController
-
-    def index
-        require 'aws-sdk'
-        @s3_client = Aws::S3::Client.new
-        @blobs = @s3_client.list_objects_v2({bucket: ENV['HUGO_PACKET_BUCKET'], prefix: ENV['HUGO_PACKET_PREFIX']})
-    end
+  def index
+    require 'aws-sdk'
+    @s3_client = Aws::S3::Client.new
+    @blobs = @s3_client.list_objects_v2({bucket: ENV['HUGO_PACKET_BUCKET'], prefix: ENV['HUGO_PACKET_PREFIX']})
+  end
 end
