3eabf5723bb92022b76e467c7f0d7e23c7ded68b
Victoria Garcia <v.elisabeth.garcia@gmail.com>
2020-07-01 04:51:45 -0700

    Added missing copyright notifications; revised Chicago production seeds to make this story and other more testable; Reduced entanglement in MembershipOffer slightly, and added a test.

--- db/seeds/chicago/production.seeds.rb
+++ db/seeds/chicago/production.seeds.rb
@@ -2,6 +2,7 @@
 #
 # Copyright 2019 Matthew B. Gray
 # Copyright 2019 Chris Rose
+# Copyright 2020 Victoria Garcia
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,8 +20,6 @@ Membership.create!(
   name: "Donor",
   price: Money.new(20_00),
   description: "With our thanks!",
-  can_attend: false,
-
 )
 
 Membership.create!(
@@ -33,5 +32,42 @@ Membership.create!(
   name: "Star",
   price: Money.new(500_00),
   description: "Will convert to an attending membership automatically if you vote in Site Selection in 2020, and hing cool for you at the convention! (Shhh…it’s a surprise!)",
+)
+
+# As of today, July 1, 2020, prices listed for the memberships below are
+# straight-up fictional, and the years and descriptions are ganked, with updated cutoff years, from ConZealand. -VEG
+
+Membership.create!(
+  name: "adult",
+  price: Money.new(400_00),
+  description: "Full membership for a person who will be physically attending the convention.",
   can_attend: true,
+)
+
+Membership.create!(
+  name: "kid_in_tow",
+  price: Money.new(50_00),
+  description: "Attending membership for a child who will be in the company of their guardian at all times.  Kids-in-tow cannot nominate, vote, or site-select.",
+  can_attend: true,
+)
+
+Membership.create!(
+  name: "child",
+  price: Money.new(100_00),
+  description: "Attending membership for a child born after 2007. Child members are allowed to be away from their guardians at times, but cannot nominate, vote, or site-select.",
+  can_attend: true,
+)
+
+Membership.create!(
+  name: "young_adult",
+  price: Money.new(250_00),
+  description: "Full, attending membership for a child born after 2002. Young adult members are expected to be on their own at times, and can nominate, vote, and site-select.",
+  can_attend: true,
+)
+
+Membership.create!(
+  name: "supporting",
+  price: Money.new(100_00),
+  description: "Voting, nominiating, and site-selecting membership for a person who will not be physically attending the convention.",
+  can_attend: false,
 )
