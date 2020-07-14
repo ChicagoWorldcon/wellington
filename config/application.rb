599e6a3a098e217483faca1854e98f85492b3cae
Victoria Garcia <v.elisabeth.garcia@gmail.com>
2020-07-14 01:57:00 -0700

    Change @con_city-related stuff removed from Application.rb and replaced with apparatus for making locale directory names that use a given Worldcon's number.  Creation of all necessary subdirectories within locales.

--- config/application.rb
+++ config/application.rb
@@ -54,14 +54,17 @@ module Conzealand
     end
 
     # Configure the name of the host city
-    config.con_city = (ENV["WORLDCON_CITY"] || "wellington").downcase
+    config.worldcon_number = (ENV["WORLDCON_NUMBER"] || "80").strip
 
     # Configure the location of the en.yml file used for i18n translation such
     # that it will serve con-specific text.  Note that this will NOT override
     # the location used by outside gems, which is why devise.en.yml has to be
     # where it is.
-    @con_city_folder = config.con_city.gsub(/[^a-z-]/i, '').downcase
-    config.i18n.load_path += Dir[Rails.root.join('config','locales', @con_city_folder, '*.{rb,yml}')]
+    @con_translation_folder = "worldcon" + config.worldcon_number
+    config.i18n.load_path += Dir[Rails.root.join('config','locales', @con_translation_folder, '*.{rb,yml}')]
+
+    # Convention-specific details that are not required for the actual
+    # configuration process come in here.
     config.convention_details = ConventionDetails::Chicago.new
     #config.i18n.default_locale = (ENV["WORLDCON_CITY"] || "en").downcase.to_sym
     #config.i18n.fallbacks = [:en]
