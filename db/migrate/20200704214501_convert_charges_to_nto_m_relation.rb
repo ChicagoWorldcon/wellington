e7efd686dc33565e2d4d9ed94a7fa44e7286d4c8
Victoria Garcia <v.elisabeth.garcia@gmail.com>
2020-07-24 18:01:13 -0700

    fix typo in migration.

--- db/migrate/20200704214501_convert_charges_to_nto_m_relation.rb
+++ db/migrate/20200704214501_convert_charges_to_nto_m_relation.rb
@@ -2,7 +2,7 @@ class ConvertChargesToNtoMRelation < ActiveRecord::Migration[6.0]
   def change
     create_table :reservation_charges do |t|
       t.references :reservation, index: true, null: false, foreign_key: true
-      t.reference :charge, index: true, null: false, foreign_key: true
+      t.references :charge, index: true, null: false, foreign_key: true
       t.column :portion, :integer, null: false
       t.monetize :portion
     end
