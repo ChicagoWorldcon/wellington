f9784c5afc13088a99f2c96ecbd8a24f1eb0a69a
Victoria Garcia <v.elisabeth.garcia@gmail.com>
2020-12-14 21:46:45 -0800

    basic routing from cart icon to cart stub works.

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-class CartsController < ApplicationController
+class CartController < ApplicationController
 
   def access_cart
       render "reservations/cart"
