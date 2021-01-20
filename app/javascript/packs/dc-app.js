0e3e134de94b04afd63e01739dff61e315f0b759
fnbauer <fred@bauer.xyz>
2021-01-20 14:31:22 -0500

    more fix

--- app/javascript/packs/dc-app.js
+++ app/javascript/packs/dc-app.js
@@ -1,5 +1,6 @@
 /*
  * Copyright 2020 Matthew B. Gray
+ * Copyright 2021 Fred Bauer
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +14,19 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/* eslint no-console:0 */
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+//
+// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
+// layout file, like app/views/layouts/application.html.erb
 
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
 // Rails defaults
 // Register theme with webpacker, allows us to use styles with stylesheet_pack_tag
 import ujs from '@rails/ujs';
