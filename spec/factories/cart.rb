a3840706d584c066ce3a6b55ea335a3cad472873
Victoria Garcia <v.elisabeth.garcia@gmail.com>
2021-01-31 20:22:42 -0800

    Beginning of factory creation for cart and cart_items.

--- spec/factories/cart.rb
+++ spec/factories/cart.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :cart do
+    status {pending}
 
   end
 end
--- spec/factories/cart_items.rb
+++ spec/factories/cart_items.rb
@@ -1,5 +1,30 @@
 FactoryBot.define do
   factory :cart_item do
-    
+    kind {"membership"}
+    later {false}
+    available {true}
+    created_at { 1.week.ago }
+
+    after(:build) do |new_cart_item, evaluator|
+      new_cart_item.chicago_contact = create(:chicago_contact, cart_item: new_cart_item)
+    end
+
+
+
+    trait :with_expired_membership do
+    end
+
+    trait :with_active_membership do
+    end
+
+    trait :saved_for_later do
+    end
+
+    trait :costs_nothing do
+    end
+
+    trait :unavailable do
+    end
+
   end
 end
