58c2b5c9be624c43f22041e7cf4c1117539d1244
fnbauer <fred@bauer.xyz>
2021-02-01 17:17:55 -0500

    Change hardcoding from Chi to DC

--- app/queries/export/membership_csv.rb
+++ app/queries/export/membership_csv.rb
@@ -1,30 +1,22 @@
 # frozen_string_literal: true
 
 # Copyright 2020 Matthew B. Gray
+# Copyright 2021 Fred Bauer
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# 1-Feb-21 FNB Change hard-coding from Chicago to DC
 
 require "csv"
 
 class Export::MembershipCsv
   def call
-    return if ChicagoContact.none?
+    return if DcContact.none?
 
     buff = StringIO.new
     csv = CSV.new(buff)
 
     csv << Export::MembershipRow::HEADINGS
-    contacts = ChicagoContact.joins(Export::MembershipRow::JOINS).eager_load(Export::MembershipRow::JOINS)
+    contacts = DcContact.joins(Export::MembershipRow::JOINS).eager_load(Export::MembershipRow::JOINS)
     contacts.merge(Claim.active).find_each do |contact|
       csv << Export::MembershipRow.new(contact).values
     end
--- app/queries/export/membership_row.rb
+++ app/queries/export/membership_row.rb
@@ -1,18 +1,10 @@
 # frozen_string_literal: true
 
 # Copyright 2020 Matthew B. Gray
+# Copyright 2021 Fred Bauer
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Licensed under the Apache License, Version 2.0 (the "License")
+# 1-Feb-21 Change hard-coding from Chicago to DC
 
 class Export::MembershipRow
   # JOINS describe fields needed to be preloaded on Detail for speed
@@ -24,7 +16,7 @@ class Export::MembershipRow
     ]
   }.freeze
 
-  CONTACT_KEYS = ChicagoContact.new.attributes.keys.freeze
+  CONTACT_KEYS = DcContact.new.attributes.keys.freeze
 
   HEADINGS = [
     "membership_number",
