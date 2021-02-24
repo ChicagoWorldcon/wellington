f2247762cae0106577d50b349df6afe2198bc471
Victoria Garcia <v.elisabeth.garcia@gmail.com>
2021-02-23 19:39:59 -0800

    Addition of cart-item processing method to ReservationsHelper

--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
-
+#
 # Copyright 2019 Matthew B. Gray
+# Copyright 2021 Victoria Garcia
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -53,5 +54,29 @@ module ReservationsHelper
     end
   end
 
+  def self.process_reservation_cart_item(c_contact, c_membership)
+    #TODO: Make sure this circularity is actually necessary.
 
+    # What this does:
+    #    1. Via ClaimMembership:
+    #       a. Assigns a membership number
+    #       b. Creates a reservation with the state: INSTALLMENT
+    #          (unless) the membership's price is zero, and
+    #          with a date-stamp
+    #       c. Creates an Order with a date-stamp
+    #       d. Creates a Claim with a date-stamp
+    new_reservation = current_user.transaction do
+      service = ClaimMembership.new(c_membership, customer: current_user)
+      new_reservation = service.call
+      c_contact.claim = new_reservation.active_claim
+      c_contact.save!
+      new_reservation
+    end
+
+    yield new_reservation
+  end
+
+  def process_reservation_cart_item(c_item)
+    ReservationsHelper.process_reservation_cart_item(c_item)
+  end
 end
