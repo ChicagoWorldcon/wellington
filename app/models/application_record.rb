965a7ca417295d0b667ad467f99143fa800de774
fnbauer <fred@bauer.xyz>
2021-03-09 14:18:15 -0500

    trim spaces

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,23 +1,16 @@
 # frozen_string_literal: true
 
 # Copyright 2018 Matthew B. Gray
+# Copyright 2021 Fred Bauer
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Licensed under the Apache License, Version 2.0
+# 9-Mar-2021 FNB Addd strip_attributes to remove all leading and trailing spaces, and strip blank vales to NIL.
 
 # ApplicationRecord is an abstract class which anything which saves to Postgres relies on.
 # It's configured through config/database.yml and inherited by most things in the models directory
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  strip_attributes
 
   protected
 
