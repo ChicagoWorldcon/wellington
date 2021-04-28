a5466255d50a876a9b1486e2f853244025103e97
fnbauer <fred@bauer.xyz>
2021-04-28 18:20:03 -0400

    Allow HTML in finalist names

--- app/javascript/finalists/finalist.vue
+++ app/javascript/finalists/finalist.vue
@@ -1,16 +1,10 @@
 // Copyright 2020 Matthew B. Gray
+// Copyright 2021 Fred Bauer
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// 28-APR-21 FNB allow finalist names to contain HTML for nice formatting
+
+// NOTE: Changes to VUE require rails server restart!
 
 <template>
   <li class="finalist-component list-group-item">
@@ -23,7 +17,7 @@
         @keyup='changeRank()'
       >
       <span v-bind:class="{ 'text-danger': invalid }">
-        {{ finalist.name }}
+        <div v-html="finalist.name"></div>
       </span>
     </div>
 
