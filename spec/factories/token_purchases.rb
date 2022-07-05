8bf13a08559b96435a931dc0929b2b75f3a1a146
Chris Rose <offline@offby1.net>
2022-07-05 07:40:54 -0700

    test: site selection model testing

--- spec/factories/token_purchases.rb
+++ spec/factories/token_purchases.rb
@@ -1,6 +1,12 @@
 FactoryBot.define do
   factory :token_purchase do
     site_selection_token { nil }
-    reservation { nil }
+    reservation { create(:reservation) }
+
+    trait :with_token do
+      after(:create) do |new_token, evaluator|
+        new_token.update!(site_selection_token: evaluator.site_selection_token)
+      end
+    end
   end
 end
--- spec/models/site_selection_token_spec.rb
+++ spec/models/site_selection_token_spec.rb
@@ -1,5 +1,26 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe SiteSelectionToken, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:model) { create(:site_selection_token) }
+  it { is_expected.to be_valid }
+
+  it "should not allow a duplicate in the same election" do
+    new_one = subject.dup
+    new_one.token = "doesn't matter, it's new"
+    expect(new_one.valid?).to be_falsey
+  end
+
+  it "should be unclaimed" do
+    subject.save!
+    expect(SiteSelectionToken.unclaimed.count).to be(1)
+  end
+
+  context "when purchased" do
+    subject(:model) { create(:site_selection_token, :purchased) }
+
+    it "should be claimed" do
+      subject.save!
+      expect(SiteSelectionToken.unclaimed.count).to be(0)
+    end
+  end
 end
