cfc6a6d95cf88df48eaa7b57176ed00e2bb1dd2d
Chris Rose <offline@offby1.net>
2022-07-19 00:32:31 -0700

    Added a token charge model

--- app/models/token_purchase.rb
+++ app/models/token_purchase.rb
@@ -1,11 +1,27 @@
 class TokenPurchase < ApplicationRecord
   belongs_to :site_selection_token
   belongs_to :reservation
+  has_one :token_charge
 
   def self.for_election!(reservation, election)
     SiteSelectionToken.transaction do
       unclaimed = SiteSelectionToken.for_election(election).unclaimed.first
       create!(reservation: reservation, site_selection_token: unclaimed)
     end
+  end
+
+  scope :unpaid, -> { where.missing(:token_charge) }
+  scope :paid, -> { joins(:token_charge).where("token_charges.id is not null") }
+
+  def paid_for?
+    token_charge.present?
+  end
+
+  def charge_stripe!(charge_id:, price_cents:)
+    update!(token_charge: TokenCharge.create(
+      charge_id: charge_id,
+      charge_provider: "stripe",
+      price_cents: price_cents
+    ))
   end
 end
