2dd90c3e2b37dc9e1c80397a45189e67eb1649e1
Chris Rose <offline@offby1.net>
2022-07-29 00:01:24 -0700

    bug: fix a silly spec error

--- spec/requests/admin_spec.rb
+++ spec/requests/admin_spec.rb
@@ -2,9 +2,9 @@ require "rails_helper"
 
 RSpec.describe "Admins", type: :request do
   describe "GET /index" do
-    it "returns http success" do
+    it "forbids unauthenticated access" do
       get "/admin"
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:forbidden)
     end
   end
 end
